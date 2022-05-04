ebce676b1b2d6333eda1ca199b760751ddd82e08
jhapooja5577 <email>
2022-05-04 11:56:35 +0530

    commit2

---
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Table_1](
+	[name] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[id] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
